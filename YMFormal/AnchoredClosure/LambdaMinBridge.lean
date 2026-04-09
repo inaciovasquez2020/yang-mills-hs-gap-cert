2ff098eb6f20613ea4ee5dc4a614e90352e35161
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-09 13:48:23 -0300

    Add lambdaMin bridge shell for anchored closure monotonicity (#28)

---
+++ YMFormal/AnchoredClosure/LambdaMinBridge.lean
@@ -0,0 +1,33 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Matrix.Basic
+import YMFormal.AnchoredClosure
+
+namespace YMFormal
+namespace AnchoredClosureBridge
+
+variable {AnchoredPatch : Type*}
+variable [HasPatchLE AnchoredPatch]
+variable [HasVertices AnchoredPatch]
+variable [HasHessian AnchoredPatch]
+variable [HasLambdaMin AnchoredPatch]
+
+axiom interiorHessian :
+  (P : AnchoredPatch) ->
+  Matrix (HasVertices.Vertices P) (HasVertices.Vertices P) ℝ
+
+axiom boundaryHessian :
+  (P : AnchoredPatch) ->
+  Matrix (HasVertices.Vertices P) (HasVertices.Vertices P) ℝ
+
+axiom hessian_decomposition :
+  ∀ P : AnchoredPatch,
+    HasHessian.hessian P = interiorHessian P + boundaryHessian P
+
+axiom lambdaMin_def :
+  True
+
+axiom boundary_psd :
+  True
+
+end AnchoredClosureBridge
+end YMFormal
